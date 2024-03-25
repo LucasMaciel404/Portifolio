aa8915f9f4decf479d31cb8bf1cf89470f93bff8
Lucas Maciel <maciellucas487@gmail.com>
2024-03-25 12:17:07 -0300

    new project

--- src/components/MyCard/index.js
+++ src/components/MyCard/index.js
@@ -2,9 +2,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import git from './../svg/git.svg'
 import styled from 'styled-components';
+import './style.css';
 
 function MyCard({ img, name, discription, githube, vercel }) {
     const Scroll = styled.p` 
+    min-height: 100px;
     max-height: 100px; /* Altura máxima do parágrafo */
     overflow-y: scroll; /* Adiciona a rolagem vertical se o conteúdo exceder a altura máxima */
 
@@ -12,7 +14,7 @@ function MyCard({ img, name, discription, githube, vercel }) {
 
     return (
         <Card style={{ width: '18rem', margin: 20 }} data-aos="fade-up">
-            <Card.Img variant="top" src={img} />
+            <Card.Img variant="top" style={{ height: 100, width: 'auto', margin: 20 }} src={img} />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
@@ -20,10 +22,10 @@ function MyCard({ img, name, discription, githube, vercel }) {
                 </Card.Text>
                 <div className='row'>
                     <div className='col'>
-                        <Button variant="primary" href={vercel} target='_blank'> Visualizar </Button>
+                        <Button className={vercel == '#'? 'block-it': ''} variant={vercel == '#'? 'secondary' : 'primary'} href={vercel} target='_blank'> Visualizar </Button>
                     </div>
                     <div className='col'>
-                        <Button variant="dark" href={githube} target='_blank'> <img src={git} alt='githube' /> Githube</Button>
+                        <Button className={githube == '#'? 'block-it': ''} variant={githube == '#'? 'secondary': 'dark'} href={githube} target='_blank'> <img src={git} alt='githube' /> Githube</Button>
                     </div>
                 </div>
             </Card.Body>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import MyCard from "../components/MyCard";
 import infoBooks from './../components/img/infobooks.png'
 import booksapi from './../components/img/swagger.png'
+import emDev from './../components/img/emDev.png'
 import { Container, Projetos, Footer } from './styles/projects'
 
 function Projects() {
@@ -12,7 +13,7 @@ function Projects() {
             <Projetos >
                 <MyCard name={'Info Books'} img={infoBooks} githube={'https://github.com/LucasMaciel404/ReadBooks'} vercel={'https://read-books-puce.vercel.app/'} discription={'Projeto criado com o intuito de consumo do BooksAPI (API de livros criada por mim) e mostrar os livros de uma forma mais visual'} />
                 <MyCard name={'Books API'} img={booksapi} githube={'https://github.com/LucasMaciel404/Books-API'} vercel={'https://booksapi-o7wo.onrender.com/doc/'} discription={'Este projeto consiste no desenvolvimento de uma API RESTful construída com Node.js e Express, integrada a um banco de dados PostgreSQL. O objetivo principal é fornecer uma interface programática para realizar operações CRUD (Create, Read, Update, Delete) em uma base de dados específica, possibilitando a manipulação de registros.'} />
-
+                <MyCard name={'Agenda de Contatos'} img={emDev} githube={'https://github.com/LucasMaciel404/Agenda-de-contatos.git'} vercel={'#'} discription={'Agenda de contatos com login e cadastro de usuarios com compatibilidade a navegadores mais antigos, tratamento de erros e proteção a ataque CSRF'} />
             </Projetos>
             <Footer><p> {'<'} Este portifolio é um projeto responsivo feito em React e Bootstrap. {'/>'} </p></Footer>
         </Container>
