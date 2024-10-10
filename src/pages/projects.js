a43cfc88092749b890c66ac132a2eb30a3482f3b
Lucas Maciel <maciellucas487@gmail.com>
2024-10-10 19:43:36 -0300

    update projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -18,49 +18,46 @@ function Projects() {
                 <MyCard
                     name={'Barber Guide'}
                     img={barberGuide}
-                    githube={'https://github.com/LucasMaciel404/BarberGuide'}
-                    vercel={'https://barber-guide.vercel.app/'}
+                    githube={'#'}
+                    vercel={'https://barber-guide.web.app/'}
                     discription={'Projeto criado para marcar horário com barbeiros e acompanhar em tempo real a fila de espera'}
                 />
                 <MyCard
                     name={'Digital Store'}
                     img={digitalStore}
-                    githube={'https://github.com/LucasMaciel404/DigitalStore'}
-                    vercel={'https://digital-store.vercel.app/'}
+                    githube={'https://github.com/LucasMaciel404/geracao-tec-store.git'}
+                    vercel={'https://geracao-tec-store.vercel.app/'}
                     discription={'Frontend de uma loja online, onde é possível visualizar e filtrar produtos por categorias'}
                 />
                 <MyCard
                     name={'Sou Mais Landing Page'}
                     img={souMais}
-                    githube={'https://github.com/LucasMaciel404/SouMaisLanding'}
-                    vercel={'https://sou-mais-landing-page.vercel.app/'}
+                    githube={'https://github.com/LucasMaciel404/Guia-Sou-Plus.git'}
+                    vercel={'https://guia-sou-plus.vercel.app/'}
                     discription={'Landing page criada para apresentar uma nova plataforma de gestão para a empresa Sou Mais'}
                 />
                 <MyCard
                     name={'Slide Exercise'}
                     img={slideExercise}
-                    githube={'https://github.com/LucasMaciel404/SlideExercise'}
-                    vercel={'https://slide-exercise.vercel.app/'}
+                    githube={'https://github.com/LucasMaciel404/ladinpage-Moda.git'}
+                    vercel={'https://ladinpage-moda.vercel.app/'}
                     discription={'Uma landing page que criei para exercitar a criação de slides animados'}
                 />
                 <MyCard
                     name={'Audio to Text Number Guessing Game'}
                     img={audioToText}
-                    githube={'https://github.com/LucasMaciel404/AudioToTextGame'}
-                    vercel={'https://audio-to-text-game.vercel.app/'}
+                    githube={'https://github.com/LucasMaciel404/Acerte-o-numero.git'}
+                    vercel={'https://acerte-o-numero-iota.vercel.app/'}
                     discription={'Site criado para converter áudio em texto e realizar um jogo de adivinhação de número sorteado'}
                 />
                 <MyCard
                     name={'Digital Menu'}
                     img={digitalMenu}
-                    githube={'https://github.com/LucasMaciel404/DigitalMenu'}
-                    vercel={'https://digital-menu.vercel.app/'}
+                    githube={'https://github.com/LucasMaciel404/Cardapio-digital.git'}
+                    vercel={'https://cardapio-digital-d8g81rdjl-lucasmaciel404s-projects.vercel.app/'}
                     discription={'Cardápio digital interativo para restaurantes e bares, com visual atraente e fácil de usar'}
                 />
             </Projetos>
-            <Footer>
-                <p>{'<'} Este portifolio é um projeto responsivo feito em React e Bootstrap. {'/>'} </p>
-            </Footer>
         </Container>
     );
 }
--- src/pages/styles/projects.js
+++ src/pages/styles/projects.js
@@ -3,14 +3,15 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding: 2em;
-    padding-buttom: 50px;
+    padding-bottom: 50px;
+
 `;
 export const Projetos = styled.div`
     margin: 0 auto;  
     display: flex;
     justify-content: center;
     align-items: start;
-    width: 80vw;
+    width: 90vw;
     height: 50vh;
 
     flex-wrap: wrap;
@@ -22,6 +23,7 @@ export const Footer = styled.footer`
     right: 0;
     left: 0;
     color: #303030;
+    background-color: #f5f5f5;
     p{
         margin: 0 auto;
         padding: 1em;
